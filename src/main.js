ac04fc0cc27292460beadf4c65acdf278e7494fb
LordeGeorge <george.ken.127@gmail.com>
2023-12-21 12:27:38 +0300

    fixes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router.js';
 
-const app = createApp({});
+const app = createApp(App);
+
 app.use(router);
+
 app.mount('app');
