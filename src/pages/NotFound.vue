5c99fddc0de06d3d6250dab087f29f0c14174638
LordeGeorge <george.ken.127@gmail.com>
2023-12-21 16:16:08 +0300

    components set and routes to those components initialised

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,3 @@
+<template>
+    nOT fOUND
+</template>
---
+++ src/pages/coaches/CoachesList.vue
@@ -0,0 +1,11 @@
+<template>
+    <section>
+        <div class="controls">
+            <button>Refresh</button>
+            <router-link to="/register">Register as Coach</router-link>
+        </div>
+    </section>
+    <section>
+        List Of Coaches
+    </section>
+</template>
